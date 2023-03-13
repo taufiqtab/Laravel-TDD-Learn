58eb1d4c7984bb1880fde79abb950c88b7ed974e
Taufiq Abdullah <taufiqtab77@gmail.com>
2023-03-13 11:21:26 +0700

    update test case

---
+++ app/Http/Controllers/BismillahController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class BismillahController extends Controller
+{
+    public static function add($a, $b){
+        return $a + $b;
+    }
+}
--- tests/Feature/BismillahTest.php
+++ tests/Feature/BismillahTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Http\Controllers\BismillahController;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -17,5 +18,23 @@ class BismillahTest extends TestCase
         $response->assertStatus(200);
         $response->assertSee('Judul Blog');
         $response->assertViewIs('articles.index');
+    }
+
+    //Negative Test
+    public function test_cuma_iseng_doang() : void {
+        $response = $this->get('/ulala');
+        $response->assertStatus(404);
+    }
+
+    //Unit Test Function Positive Result
+    public function test_add_function(){
+        $result = BismillahController::add(10,5);
+        $this->assertEquals(15, $result);
+    }
+
+    //Unit Test Function Negative Result
+    public function test_add_negative_function(){
+        $result = BismillahController::add(10,5);
+        $this->assertNotEquals(10, $result);
     }
 }
